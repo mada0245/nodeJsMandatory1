d5b760d95be123af63aa059136dcdf39e51b1e7a
Loghin Petru Madalin <madapetru70@gmail.com>
2023-03-29 19:35:59 +0200

    Installing dependencies

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/frontEnd/loginPage/loginPage.html");
+});
+
+const PORT = 8080;
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    console.log("Server is running on port", PORT);
+});
